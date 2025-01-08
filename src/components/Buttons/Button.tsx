a8dd789fde388bc57aff0b506ec2942f9476b731
Gabriel Poeta <95362336+gabrielpoetaa@users.noreply.github.com>
2025-01-08 10:16:33 -0500

    fix: update hasBg={false} background and text color

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -11,16 +11,17 @@ export const Button = ({
   onClick,
   children,
   disabled = false,
+  type = "button",
 }: ButtonProps) => {
   return (
     <button
-      type="button"
+      type={type}
       className={`text-white text-[14px] my-2 px-4 py-2 text-sm rounded-lg transition-colors
     duration-300 transform
     ${
       hasBg
         ? "bg-[#d6b56e] hover:bg-opacity-80"
-        : "border border-solid border border-[#d6b56e] text-[#ba8638] hover:bg-[#d6b56e] hover:text-white"
+        : "border bg-transparent border-solid border border-[#d6b56e] text-[#c99b46] hover:bg-[#d6b56e] hover:text-white"
     }
     ${
       disabled
